3f78027d87b1df8f5defe31dfe0cccd5e6a05a09
Simon Asika <asika32764@gmail.com>
2022-12-27 21:44:10 +0800

    location seeder

--- resources/migrations/2022122708280011_LocationInit.php
+++ resources/migrations/2022122708280011_LocationInit.php
@@ -12,18 +12,27 @@ declare(strict_types=1);
 namespace App\Migration;
 
 use App\Entity\Location;
+use App\Enum\LocationType;
+use App\Service\LocationService;
+use Lyrasoft\Luna\Services\LocaleService;
 use Windwalker\Core\Console\ConsoleApplication;
 use Windwalker\Core\Migration\Migration;
 use Windwalker\Database\Schema\Schema;
+use Windwalker\ORM\Nested\Position;
+use Windwalker\ORM\NestedSetMapper;
+use Windwalker\ORM\ORM;
+use Windwalker\Stream\Stream;
+
+use function Windwalker\chronos;
 
 /**
  * Migration UP: 2022122708280011_LocationInit.
  *
- * @var Migration          $mig
+ * @var Migration $mig
  * @var ConsoleApplication $app
  */
 $mig->up(
-    static function () use ($mig) {
+    static function (ORM $orm) use ($mig, &$importLocations) {
         $mig->createTable(
             Location::class,
             function (Schema $schema) {
@@ -37,7 +46,8 @@ $mig->up(
                 $schema->varchar('region');
                 $schema->varchar('subregion');
                 $schema->varchar('title');
-                $schema->char('code')->length(2);
+                $schema->varchar('native');
+                $schema->char('code')->length(32);
                 $schema->char('code3')->length(3);
                 $schema->text('address_format');
                 $schema->bool('postcode_required');
@@ -59,6 +69,16 @@ $mig->up(
                 $schema->addIndex('code3');
             }
         );
+
+        /** @var NestedSetMapper $mapper */
+        $mapper = $orm->mapper(Location::class);
+        $mapper->createRootIfNotExist(
+            [
+                'type' => LocationType::ROOT(),
+            ]
+        );
+
+        $importLocations($orm);
     }
 );
 
@@ -71,3 +91,87 @@ $mig->down(
         $mig->dropTables(Location::class);
     }
 );
+
+$importLocations = function (ORM $orm) use ($mig, $app) {
+    $locationService = $app->service(LocationService::class);
+    $zipFile = __DIR__ . '/data/locations.zip';
+    $countriesData = Stream::wrap("zip://$zipFile#countries.json", Stream::MODE_READ_ONLY_FROM_BEGIN)
+        ->getContents();
+    $zonesData = Stream::wrap("zip://$zipFile#zones.json", Stream::MODE_READ_ONLY_FROM_BEGIN)
+        ->getContents();
+
+    $countries = json_decode($countriesData, true);
+    $zones = json_decode($zonesData, true);
+
+    /** @var NestedSetMapper<Location> $mapper */
+    $mapper = $orm->mapper(Location::class);
+
+    $countriesMapping = [];
+
+    $regions = array_unique(array_column($countries, 'region'));
+
+    foreach ($regions as $region) {
+        $locationService->getOrCreateContinent($region);
+    }
+
+    /**
+     * @var array{
+     *     id: int, region: string, subregion: string, title: string,
+     *     native: string, code: string, code3: string, address_format: string,
+     *     postcode_required: int, call_prefix: int, has_states: int
+     *     } $country
+     */
+    foreach ($countries as $country) {
+        $item = $mapper->createEntity();
+
+        $continent = $locationService->getOrCreateContinent($country['region']);
+
+        $item->setType(LocationType::COUNTRY());
+        $item->setTitle($country['title']);
+        $item->setNative($country['native']);
+        $item->setRegion($country['region']);
+        $item->setSubregion($country['subregion']);
+        $item->setCode($country['code']);
+        $item->setCode3($country['code3']);
+        $item->setAddressFormat($country['address_format']);
+        $item->setPostcodeRequired((bool) $country['postcode_required']);
+        $item->setCallPrefix((string) $country['call_prefix']);
+        $item->setHasStates((bool) $country['has_states']);
+
+        $mapper->setPosition($item, $continent->getId(), Position::LAST_CHILD);
+
+        $location = $mapper->createOne($item);
+
+        $countriesMapping[$country['id']] = $location;
+
+        $mig->outCounting();
+    }
+
+    foreach (array_chunk($zones, 500) as $zoneChunk) {
+        $query = $orm->insert(Location::class);
+        $query->columns('type', 'parent_id', 'title', 'code', 'address_format', 'created');
+
+        /** @var array{ id: int, country_id: int, title: string, code: string } $zone */
+        foreach ($zoneChunk as $zone) {
+            /** @var Location $country */
+            $country = $countriesMapping[$zone['country_id']];
+
+            $query->values(
+                [
+                    LocationType::STATE(),
+                    $country->getId(),
+                    $zone['title'],
+                    $zone['code'],
+                    '',
+                    chronos()
+                ]
+            );
+
+            $mig->outCounting();
+        }
+
+        $query->execute();
+    }
+
+    $mapper->rebuild();
+};
---
+++ src/Enum/LocationType.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2021 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+use MyCLabs\Enum\Enum;
+use Windwalker\Utilities\Enum\EnumTranslatableInterface;
+use Windwalker\Utilities\Enum\EnumTranslatableTrait;
+use Windwalker\Utilities\Contract\LanguageInterface;
+
+/**
+ * The LocationType enum class.
+ *
+ * @method static $this ROOT()
+ * @method static $this CONTINENT()
+ * @method static $this COUNTRY()
+ * @method static $this STATE()
+ * @method static $this CITY()
+ */
+class LocationType extends Enum implements EnumTranslatableInterface
+{
+    use EnumTranslatableTrait;
+
+    public const ROOT = 'root';
+
+    public const CONTINENT = 'continent';
+
+    public const COUNTRY = 'country';
+
+    public const STATE = 'state';
+
+    public const CITY = 'city';
+
+    /**
+     * Creates a new value of some type
+     *
+     * @psalm-pure
+     *
+     * @param  mixed  $value
+     *
+     * @psalm-param T $value
+     * @throws \UnexpectedValueException if incompatible type is given.
+     */
+    public function __construct(mixed $value)
+    {
+        parent::__construct($value);
+    }
+
+    public function trans(LanguageInterface $lang, ...$args): string
+    {
+        return $lang->trans('shopgo.location.type.' . $this->getKey());
+    }
+}
