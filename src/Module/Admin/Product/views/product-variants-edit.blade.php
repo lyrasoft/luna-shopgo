1ab0d4bb89ad2da1069c1bb5f93c39b2f9364f2b
Simon Asika <asika32764@gmail.com>
2023-01-25 12:32:49 +0800

    variant edit wip

---
+++ src/Module/Admin/Product/views/product-variants-edit.blade.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\view;
+
+/**
+ * Global variables
+ * --------------------------------------------------------------
+ * @var $app       AppContext      Application context.
+ * @var $vm        object          The view model object.
+ * @var $uri       SystemUri       System Uri information.
+ * @var $chronos   ChronosService  The chronos datetime service.
+ * @var $nav       Navigator       Navigator object to build route.
+ * @var $asset     AssetService    The Asset manage service.
+ * @var $lang      LangService     The language translation service.
+ */
+
+use Windwalker\Core\Application\AppContext;
+use Windwalker\Core\Asset\AssetService;
+use Windwalker\Core\DateTime\ChronosService;
+use Windwalker\Core\Language\LangService;
+use Windwalker\Core\Router\Navigator;
+use Windwalker\Core\Router\SystemUri;
+
+?>
+
+<product-variants-edit-app id="product-variants-edit-app">
+
+</product-variants-edit-app>
+
+<script type="text/x-template">
+
+</script>
