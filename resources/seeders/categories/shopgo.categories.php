988eb6ea1637ce808da82750f186d5a101528314
Simon Asika <asika32764@gmail.com>
2025-10-24 17:22:47 +0800

    Use new categories data

---
+++ resources/seeders/categories/shopgo.categories.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'product' => [
+        'max_level' => 2,
+        'number' => 30,
+    ],
+    'attribute' => [
+        'max_level' => 1,
+        'number' => 10,
+    ],
+];
