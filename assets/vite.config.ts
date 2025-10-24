77ec0a892a436e3ad60eb05d4e36f8ae0f724bf2
Simon Asika <asika32764@gmail.com>
2025-10-25 01:51:02 +0800

    misc

--- assets/vite.config.ts
+++ assets/vite.config.ts
@@ -2,6 +2,7 @@ import vuePlugin from '@vitejs/plugin-vue';
 import { resolve } from 'node:path';
 import dts from 'unplugin-dts/vite';
 import { defineConfig } from 'vite';
+import vueComponentOverride from 'vite-plugin-vue-component-override/plugin';
 
 export default defineConfig(() => {
   return {
@@ -29,24 +30,36 @@ export default defineConfig(() => {
 
             return 'assets/[name][extname]';
           },
+          // manualChunks(id, meta) {
+          //   const file = id.split('?').shift();
+          //   const src = resolve('./src');
+          //
+          //   if (file.endsWith('.vue') && file.startsWith(src)) {
+          //     const relativePath = file.substring(src.length + 1);
+          //
+          //     return relativePath;
+          //   }
+          // }
         },
         external: [
           '@windwalker-io/unicorn-next',
           '@lyrasoft/ts-toolkit',
           /^@lyrasoft\/ts-toolkit/,
           '@unicorn/*',
+          'sweetalert',
           'bootstrap',
           'sortablejs',
           '@asika32764/vue-animate',
           'bootstrap',
           'vue',
           'vue-draggable-plus',
-          'vue-multi-uploader'
+          'vue-multi-uploader',
+          // /^vite-plugin-vue-component-override/,
         ]
       },
       outDir: 'dist',
       emptyOutDir: true,
-      sourcemap: true,
+      sourcemap: false,
       minify: true,
     },
     plugins: [
@@ -58,14 +71,60 @@ export default defineConfig(() => {
           compilerOptions: {
             // preserveWhitespace: false,
             whitespace: 'preserve',
-          }
+            isCustomElement: (tag: string) => {
+              return [
+                'uni-flatpickr',
+                'uni-iframe-modal'
+              ].includes(tag);
+            }
+          },
         }
       }),
 
       dts({
         tsconfigPath: resolve('./tsconfig.json'),
         bundleTypes: true,
+      }),
+
+      vueComponentOverride({
+        alias: {
+          '~shopgo/modules/additional-purchase-attachment-edit/components/AttachmentProduct.vue': 'AttachmentProduct'
+        }
       })
+
+//       {
+//         name: 'vue-override',
+//         enforce: 'pre',
+//         transform(code, id) {
+//           if (!/\.(js|ts|jsx|tsx|vue)$/.test(id)) {
+//             return null
+//           }
+//
+//           let helper = false;
+//
+//           code = code.replaceAll(/import\s+(.*?)\s+from\s+'((.*?)\.vue)'\s*(;?)/g, (match, component, uri) => {
+//             if (component.includes('__Tmp')) {
+//               return match;
+//             }
+//
+//             const tmpName = component + '__Tmp' + Math.floor(Math.random() * 100000);
+//             let replaced = `import ${tmpName} from '${uri}';\n
+// const ${component} = useUnicorn().inject('${uri}') ?? ${tmpName};`;
+//
+//             if (!helper && !code.match(/\{.*?useUnicorn.*?\}\s+from/)) {
+//               replaced = `import { useUnicorn } from '@windwalker-io/unicorn-next';\n` + replaced;
+//               helper = true;
+//             }
+//
+//             return replaced;
+//           });
+//
+//           return {
+//             code,
+//             map: null
+//           };
+//         }
+//       }
     ]
   };
 });
