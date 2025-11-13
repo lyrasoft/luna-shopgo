d499419456cf944e8bb4343d64f5983db0fe57b4
Simon Asika <asika32764@gmail.com>
2025-11-13 22:39:41 +0800

    Release version: 0.2.3

--- assets/dist/chunks/cart.js
+++ assets/dist/chunks/cart.js
@@ -223,7 +223,7 @@ function _sfc_render$a(a, e, n, t, d, u) {
     }, null, 8, _hoisted_8$6)
   ], 512);
 }
-const CascadeSelect__Tmp72230 = /* @__PURE__ */ _export_sfc(_sfc_main$a, [["render", _sfc_render$a], ["__file", "CascadeSelect.vue"]]), _sfc_main$9 = /* @__PURE__ */ defineComponent({
+const CascadeSelect__Tmp66176 = /* @__PURE__ */ _export_sfc(_sfc_main$a, [["render", _sfc_render$a], ["__file", "CascadeSelect.vue"]]), _sfc_main$9 = /* @__PURE__ */ defineComponent({
   __name: "AddressForm",
   props: /* @__PURE__ */ mergeModels({
     type: {},
@@ -239,7 +239,7 @@ const CascadeSelect__Tmp72230 = /* @__PURE__ */ _export_sfc(_sfc_main$a, [["rend
   }),
   emits: /* @__PURE__ */ mergeModels(["validated"], ["update:modelValue"]),
   setup(a, { expose: e, emit: n }) {
-    const t = resolveVueComponent("CascadeSelect", CascadeSelect__Tmp72230), d = a, u = {
+    const t = resolveVueComponent("CascadeSelect", CascadeSelect__Tmp66176), d = a, u = {
       // addressId: '',
       // locationId: 0,
       // firstname: '',
@@ -754,7 +754,7 @@ function _sfc_render$9(a, e, n, t, d, u) {
     ], 8, _hoisted_63)
   ]);
 }
-const AddressForm__Tmp3211 = /* @__PURE__ */ _export_sfc(_sfc_main$9, [["render", _sfc_render$9], ["__file", "AddressForm.vue"]]), _sfc_main$8 = /* @__PURE__ */ defineComponent({
+const AddressForm__Tmp85369 = /* @__PURE__ */ _export_sfc(_sfc_main$9, [["render", _sfc_render$9], ["__file", "AddressForm.vue"]]), _sfc_main$8 = /* @__PURE__ */ defineComponent({
   __name: "AddressFormSet",
   props: /* @__PURE__ */ mergeModels({
     user: {}
@@ -770,7 +770,7 @@ const AddressForm__Tmp3211 = /* @__PURE__ */ _export_sfc(_sfc_main$9, [["render"
   }),
   emits: ["update:payment", "update:shipping"],
   setup(a, { expose: e }) {
-    const n = resolveVueComponent("AddressForm", AddressForm__Tmp3211);
+    const n = resolveVueComponent("AddressForm", AddressForm__Tmp85369);
     e();
     const t = useModel(a, "payment"), d = useModel(a, "shipping"), u = { paymentData: t, shippingData: d, AddressForm: n };
     return Object.defineProperty(u, "__isScriptSetup", { enumerable: !1, value: !0 }), u;
@@ -798,7 +798,7 @@ function _sfc_render$8(a, e, n, t, d, u) {
     }, null, 8, ["title", "user", "modelValue", "sync-data"])
   ]);
 }
-const AddressFormSet__Tmp46532 = /* @__PURE__ */ _export_sfc(_sfc_main$8, [["render", _sfc_render$8], ["__file", "AddressFormSet.vue"]]), _sfc_main$7 = /* @__PURE__ */ defineComponent({
+const AddressFormSet__Tmp284 = /* @__PURE__ */ _export_sfc(_sfc_main$8, [["render", _sfc_render$8], ["__file", "AddressFormSet.vue"]]), _sfc_main$7 = /* @__PURE__ */ defineComponent({
   __name: "CartForm",
   props: /* @__PURE__ */ mergeModels({
     user: {},
@@ -849,7 +849,7 @@ function _sfc_render$7(a, e, n, t, d, u) {
     })
   ]);
 }
-const CartForm__Tmp33221 = /* @__PURE__ */ _export_sfc(_sfc_main$7, [["render", _sfc_render$7], ["__file", "CartForm.vue"]]), _sfc_main$6 = /* @__PURE__ */ defineComponent({
+const CartForm__Tmp56908 = /* @__PURE__ */ _export_sfc(_sfc_main$7, [["render", _sfc_render$7], ["__file", "CartForm.vue"]]), _sfc_main$6 = /* @__PURE__ */ defineComponent({
   __name: "CartListItem",
   props: {
     item: {},
@@ -1070,7 +1070,7 @@ function _sfc_render$6(a, e, n, t, d, u) {
     ])) : createCommentVNode("", !0)
   ], 8, _hoisted_1$6);
 }
-const CartListItem__Tmp34906 = /* @__PURE__ */ _export_sfc(_sfc_main$6, [["render", _sfc_render$6], ["__file", "CartListItem.vue"]]), _sfc_main$5 = /* @__PURE__ */ defineComponent({
+const CartListItem__Tmp14650 = /* @__PURE__ */ _export_sfc(_sfc_main$6, [["render", _sfc_render$6], ["__file", "CartListItem.vue"]]), _sfc_main$5 = /* @__PURE__ */ defineComponent({
   __name: "CartSidebar",
   props: /* @__PURE__ */ mergeModels({
     totals: {},
@@ -1290,7 +1290,7 @@ function _sfc_render$5(a, e, n, t, d, u) {
     ])
   ]);
 }
-const CartSidebar__Tmp47500 = /* @__PURE__ */ _export_sfc(_sfc_main$5, [["render", _sfc_render$5], ["__file", "CartSidebar.vue"]]), _sfc_main$4 = /* @__PURE__ */ defineComponent({
+const CartSidebar__Tmp79336 = /* @__PURE__ */ _export_sfc(_sfc_main$5, [["render", _sfc_render$5], ["__file", "CartSidebar.vue"]]), _sfc_main$4 = /* @__PURE__ */ defineComponent({
   __name: "PaymentItem",
   props: {
     payment: {},
@@ -1396,7 +1396,7 @@ function _sfc_render$4(a, e, n, t, d, u) {
     })
   ], 2);
 }
-const PaymentItem__Tmp28337 = /* @__PURE__ */ _export_sfc(_sfc_main$4, [["render", _sfc_render$4], ["__file", "PaymentItem.vue"]]), _sfc_main$3 = /* @__PURE__ */ defineComponent({
+const PaymentItem__Tmp44907 = /* @__PURE__ */ _export_sfc(_sfc_main$4, [["render", _sfc_render$4], ["__file", "PaymentItem.vue"]]), _sfc_main$3 = /* @__PURE__ */ defineComponent({
   __name: "PaymentSelector",
   props: /* @__PURE__ */ mergeModels({
     payments: {},
@@ -1409,7 +1409,7 @@ const PaymentItem__Tmp28337 = /* @__PURE__ */ _export_sfc(_sfc_main$4, [["render
   }),
   emits: ["update:modelValue"],
   setup(a, { expose: e }) {
-    const n = resolveVueComponent("PaymentItem", PaymentItem__Tmp28337);
+    const n = resolveVueComponent("PaymentItem", PaymentItem__Tmp44907);
     e();
     const d = { paymentId: useModel(a, "modelValue"), PaymentItem: n };
     return Object.defineProperty(d, "__isScriptSetup", { enumerable: !1, value: !0 }), d;
@@ -1448,7 +1448,7 @@ function _sfc_render$3(a, e, n, t, d, u) {
     ]))
   ]);
 }
-const PaymentSelector__Tmp46513 = /* @__PURE__ */ _export_sfc(_sfc_main$3, [["render", _sfc_render$3], ["__file", "PaymentSelector.vue"]]), _sfc_main$2 = /* @__PURE__ */ defineComponent({
+const PaymentSelector__Tmp62157 = /* @__PURE__ */ _export_sfc(_sfc_main$3, [["render", _sfc_render$3], ["__file", "PaymentSelector.vue"]]), _sfc_main$2 = /* @__PURE__ */ defineComponent({
   __name: "ShippingItem",
   props: {
     shipping: {},
@@ -1561,7 +1561,7 @@ function _sfc_render$2(a, e, n, t, d, u) {
     })
   ], 2);
 }
-const ShippingItem__Tmp29158 = /* @__PURE__ */ _export_sfc(_sfc_main$2, [["render", _sfc_render$2], ["__file", "ShippingItem.vue"]]), _sfc_main$1 = /* @__PURE__ */ defineComponent({
+const ShippingItem__Tmp86504 = /* @__PURE__ */ _export_sfc(_sfc_main$2, [["render", _sfc_render$2], ["__file", "ShippingItem.vue"]]), _sfc_main$1 = /* @__PURE__ */ defineComponent({
   __name: "ShippingSelector",
   props: /* @__PURE__ */ mergeModels({
     shippings: {},
@@ -1573,7 +1573,7 @@ const ShippingItem__Tmp29158 = /* @__PURE__ */ _export_sfc(_sfc_main$2, [["rende
   }),
   emits: ["update:modelValue"],
   setup(a, { expose: e }) {
-    const n = resolveVueComponent("ShippingItem", ShippingItem__Tmp29158);
+    const n = resolveVueComponent("ShippingItem", ShippingItem__Tmp86504);
     e();
     const d = { shippingId: useModel(a, "modelValue"), ShippingItem: n };
     return Object.defineProperty(d, "__isScriptSetup", { enumerable: !1, value: !0 }), d;
@@ -1612,15 +1612,15 @@ function _sfc_render$1(a, e, n, t, d, u) {
     ]))
   ]);
 }
-const ShippingSelector__Tmp40431 = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render$1], ["__file", "ShippingSelector.vue"]]), _sfc_main = /* @__PURE__ */ defineComponent({
+const ShippingSelector__Tmp66453 = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render$1], ["__file", "ShippingSelector.vue"]]), _sfc_main = /* @__PURE__ */ defineComponent({
   __name: "CartApp",
   props: {
     user: {},
     checkoutData: {}
   },
   setup(a, { expose: e }) {
-    resolveVueComponent("AddressForm", AddressForm__Tmp3211);
-    const n = resolveVueComponent("AddressFormSet", AddressFormSet__Tmp46532), t = resolveVueComponent("CartForm", CartForm__Tmp33221), d = resolveVueComponent("CartListItem", CartListItem__Tmp34906), u = resolveVueComponent("CartSidebar", CartSidebar__Tmp47500), o = resolveVueComponent("PaymentSelector", PaymentSelector__Tmp46513), r = resolveVueComponent("ShippingSelector", ShippingSelector__Tmp40431);
+    resolveVueComponent("AddressForm", AddressForm__Tmp85369);
+    const n = resolveVueComponent("AddressFormSet", AddressFormSet__Tmp284), t = resolveVueComponent("CartForm", CartForm__Tmp56908), d = resolveVueComponent("CartListItem", CartListItem__Tmp14650), u = resolveVueComponent("CartSidebar", CartSidebar__Tmp79336), o = resolveVueComponent("PaymentSelector", PaymentSelector__Tmp62157), r = resolveVueComponent("ShippingSelector", ShippingSelector__Tmp66453);
     e();
     const l = a, g = ref(!1), p = ref([]), C = ref({}), S = ref([]), v = ref(l.checkoutData?.payment?.id || ""), x = ref(l.checkoutData?.payment_data || {}), y = ref(l.checkoutData?.shipping?.id || ""), B = ref(l.checkoutData?.shipping_data || {}), D = ref([]), $ = ref([]), A = ref(""), j = ref(l.checkoutData?.note || ""), P = ref(!1), Q = ref(data("partial.checkout")), H = useQueue("shopgo.cart"), E = useStack("loading");
     provide("checkoutData", l.checkoutData), provide("items", p), provide("totals", C), provide("coupons", S), provide("shippingData", B), provide("paymentData", x), provide("shippingId", y), provide("paymentId", v), provide("shippings", D), provide("payments", $), provide("loading", P), provide("code", A), provide("note", j), provide("queue", H), provide("partialCheckout", Q);
@@ -2016,7 +2016,7 @@ function _sfc_render(a, e, n, t, d, u) {
     ])
   ]);
 }
-const CartApp__Tmp25236 = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "CartApp.vue"]]), CartApp = resolveVueComponent("CartApp", CartApp__Tmp25236);
+const CartApp__Tmp48487 = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "CartApp.vue"]]), CartApp = resolveVueComponent("CartApp", CartApp__Tmp48487);
 function initApp(a) {
   useCssImport("@vue-animate");
   const e = createApp(CartApp, a);
